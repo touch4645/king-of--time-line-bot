45d7647effbd86cd0a3088c0a0ead104f41895ea
touch4645 <k.terazawa@lillyholdings.co.jp>
2022-01-20 16:37:31 +0900

    fix lineworksController

--- src/lineworksController.js
+++ src/lineworksController.js
@@ -1,10 +1,7 @@
 function main() {
     console.info({method: arguments.callee.name, status: 'run'});
-    //king of time botのenvファイルを呼び出し
-    const kingOfTimeBotOption = setOptions();
-  
     //king of time インスタンスの生成 
-    const kingOfTimeBot = new LineWorksBot(kingOfTimeBotOption);
+    const kingOfTimeBot = createKingOfTimeBot();
   
     //メッセージ作成
     const actions = [{
@@ -18,18 +15,28 @@ function main() {
       "contentText": "打刻しますか?",
       "actions": actions
     }
-  
-  
-    //スプレッドシートのアカウントIDを取得
-    const array = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
-    for (let row = 1; row < array.length; row++) {
-      //アカウントID
-      const accountId = array[row][0];
-  
-      //LineにメッセージをPOST送信
-      console.log(accountId, kingOfTimeBot.sendMessage(accountId, content).getContentText());
-    }
+
+    const accountIds = getAccountIds();
+    accountIds.forEach(accountId => {
+        kingOfTimeBot.sendMessage(accountId, content);
+    });
     console.info({method: arguments.callee.name, status: 'success'});
+}
+
+
+function getAccountIds() {
+    const array = SpreadsheetApp.getActiveSheet().getDataRange().getValues();
+    array.shift();
+    return array.map(accountId => accountId[0]);
+}
+
+
+function createKingOfTimeBot() {
+    //king of time botのenvファイルを呼び出し
+    const kingOfTimeBotOption = setOptions();
+  
+    //king of time インスタンスの生成 
+    return new LineWorksBot(kingOfTimeBotOption);
 }
   
   
